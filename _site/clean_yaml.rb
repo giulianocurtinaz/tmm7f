5e110bc46b7f6865a9c5b842562fa3e6cafb0442
Giuliano <giuliano@local>
2026-05-02 22:41:03 -0300

    Limpa repo: ignora _site, caches, scripts de manutencao, arquivos legados

--- _site/clean_yaml.rb
+++
@@ -1,8 +0,0 @@
-﻿require 'fileutils'
-
-Dir.glob('_genealogia/*.md').each do |file|
-  content = File.read(file, encoding: 'bom|utf-8')
-  clean_content = content.gsub(/[\x00-\x08\x0B-\x0C\x0E-\x1F\x7F-\x9F]/, '')
-  File.write(file, clean_content, encoding: 'utf-8')
-  puts "Cleaned: #{file}"
-end
